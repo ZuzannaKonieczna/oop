b0766a24dbd66bf3d76a4796465a386a7abfc999
Zuzanna <zuzannakonieczna29@gmail.com>
2025-06-17 14:41:05 +0200

    adding Person class

---
+++ start.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, imie , wiek):
+        self.name = imie
+        self.age = wiek
+        self.presents_list = []
+    def add_gift():
+        self.gift_list.append(prezent)
+    def pokaz_prezenty(self):
+        print(f"Prezenty dla {self.imie}:")
+        for p in self.lista_prezentow:
+            print(f"- {p.nazwa} (od {p.nadawca.imie})")
+
+zuzia = Person("Zuzia", 18)
+
+print(zuzia.age)
